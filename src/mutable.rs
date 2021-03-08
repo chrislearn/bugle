5bbfd2bc5520b084c7a5bb82cf57745a640a3ee6
Chrislearn Young <chris@acroidea.com>
2021-03-08 22:09:35 +0800

    init

--- src/mutable.rs
+++ src/mutable.rs
@@ -1,6 +1,9 @@
 use crate::{Emitter, Receiver};
 
-pub trait Mutable<'a, 'b, D> where D: Clone + 'static{
+pub trait Mutable<'a, D>
+where
+    D: Clone + 'static,
+{
     fn new(value: D) -> Self
     where
         D: Sized;
@@ -8,14 +11,14 @@ pub trait Mutable<'a, 'b, D> where D: Clone + 'static{
     fn set(&mut self, value: D);
     fn on_change<R>(&mut self, receiver: R)
     where
-        R: Receiver<Changed<'b, D>> + Send + 'a;
+        R: Receiver<Changed<D>> + Send + 'a;
     //     fn change_map<F, T, R>(&mut self, func: F) -> R
     //     where
     //         F: Fn(&D) -> T + Send + 'static,
     //         R: Mutable<T>;
 }
 #[derive(Debug, Clone, Copy)]
-pub struct Changed<'b, D> { 
-    pub old: &'b D,
-    // pub new: &'b D,
+pub struct Changed<D> {
+    pub old: D,
+    pub new: D,
 }
--- src/signal_var.rs
+++ src/signal_var.rs
@@ -3,9 +3,9 @@ use std::sync::{Arc, Weak, Mutex};
 use crate::{Emitter, Mutable, Receiver};
 use crate::mutable::Changed;
 
-pub struct SignalVar<'a, 'b, D> {
+pub struct SignalVar<'a, D> {
     value: D,
-    change_receivers: Vec<Weak<Mutex<Box<dyn Receiver<Changed<'b, D>> + 'a>>>>,
+    change_receivers: Vec<Weak<Mutex<Box<dyn Receiver<Changed<D>> + 'a>>>>,
 }
 
 // pub struct SignalVarMap<'a, D, F> {
@@ -15,7 +15,7 @@ pub struct SignalVar<'a, 'b, D> {
 // }
 
 
-impl<'a, 'b, D> SignalVar<'a, 'b, D> {
+impl<'a, D> SignalVar<'a, D> {
     pub fn new(value: D) -> Self {
         SignalVar {
             value,
@@ -23,7 +23,7 @@ impl<'a, 'b, D> SignalVar<'a, 'b, D> {
         }
     }
 }
-impl<'a, 'b, D> Mutable<'a, 'b, D> for SignalVar<'a, 'b, D> where D: Clone + 'static {
+impl<'a, D> Mutable<'a, D> for SignalVar<'a, D> where D: Clone + 'static {
     fn new(value: D) -> Self where D:Sized {
         SignalVar {
             value,
@@ -36,18 +36,18 @@ impl<'a, 'b, D> Mutable<'a, 'b, D> for SignalVar<'a, 'b, D> where D: Clone + 'st
     fn set(&mut self, value: D) {
         let old = std::mem::replace(&mut self.value, value);
         let changed = Changed {
-            old: &old,
-            // new: &self.value,
+            old: old,
+            new: self.value.clone(),
         };
         for receiver in &mut self.change_receivers {
             if let Some(receiver) = receiver.upgrade() {
                 if let Ok(mut receiver) = receiver.lock() {
-                    receiver.receive(&changed.clone());
+                    receiver.receive(&changed);
                 }
             }
         }
     }
-    fn on_change<R>(&mut self, receiver: R) where R: Receiver<Changed<'b, D>> + Send + 'a{
+    fn on_change<R>(&mut self, receiver: R) where R: Receiver<Changed<D>> + Send + 'a{
         self.change_receivers.push(Arc::downgrade(&Arc::new(Mutex::new(Box::new(receiver)))));
     }
     // fn change_map<F, T, R>(&mut self, func: F) -> R where F: Fn(&D) -> T + Send + 'static, R: Mutable<T>{
